fa9719c82b8c40dd98b2c9ff9d0fab02fc78259c
Calla Rudolph and Nathan Stewart <nathan@>
2017-06-27 09:19:39 -0700

    set src path, create form for job posting input

--- app/app.php
+++ app/app.php
@@ -1,11 +1,40 @@
 <?php
     date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/job-board.php";
 
     $app = new Silex\Application();
 
     $app->get("/", function() {
-        return ;
+        return "
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <link rel='stylesheet' href='https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css'>
+            <title>Create a job posting</title>
+        </head>
+        <body>
+            <div class='container'>
+                <h1>Tell us about the job:</h1>
+                <form action='/view_job_board'>
+                    <div class='form-group'>
+                        <label for='title'>Enter the title:</label>
+                        <input id='title' name='title' class='form-control' type='text'>
+                    </div>
+                    <div class='form-group'>
+                        <label for='description'>Enter the description:</label>
+                        <input id='description' name='description' class='form-control' type='text'>
+                    </div>
+                    <div class='form-group'>
+                        <label for='contact_info'>Enter the contact:</label>
+                        <input id='contact_info' name='contact_info' class='form-control' type='text'>
+                    </div>
+                    <button type='submit' class='btn-danger'>Create!</button>
+                </form>
+            </div>
+        </body>
+        </html>
+        ";
     });
 
     return $app;
